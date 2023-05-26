85eba135d40eb1fd4fd543eafbb79d3f48ea2067
Shweta <shwetakale144@gmail.com>
2023-05-26 20:29:38 +0530

    add active link in menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,30 +1,38 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Menu.css";
 
 export const Menu = () => {
   return (
     <nav>
       <ul className="menu-list">
-        <Link to="/" className="header-link">
-          <li className="menu-item">
+        <li className="menu-item">
+          <NavLink  to="/" className={({ isActive, isPending }) =>
+    isPending ? "menu-link" : isActive ? "active-link" : "menu-link"
+  } >
             <span>🏠 Home</span>
-          </li>
-        </Link>
-        <Link to="/user" className="header-link">
-            <li className="menu-item">
-                <span>🤖 User</span>
-            </li>
-        </Link>
-        <Link to="/company" className="header-link">
-            <li className="menu-item">
-                <span>🏢 Company</span>
-            </li>
-        </Link>
-        <Link to="/events" className="header-link">
-            <li className="menu-item">
-                <span>📅 Events</span>
-            </li>
-        </Link>
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink to="/user" className={({ isActive, isPending }) =>
+    isPending ? "menu-link" : isActive ? "active-link" : "menu-link"
+  } >
+            <span>🤖 User</span>
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink to="/company" className={({ isActive, isPending }) =>
+    isPending ? "menu-link" : isActive ? "active-link" : "menu-link"
+  } >
+            <span>🏢 Company</span>
+          </NavLink>
+        </li>
+        <li className="menu-item">
+          <NavLink to="/events" className={({ isActive, isPending }) =>
+    isPending ? "menu-link" : isActive ? "active-link" : "menu-link"
+  } >
+            <span>📅 Events</span>
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
